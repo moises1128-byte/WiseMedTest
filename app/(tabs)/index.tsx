ddc250b131e7e1f6f7b6e43feb20b4ee6da93fc2
moises <1128moises@gmail.com>
2025-01-30 19:10:02 -0400

    Add SVG support, picker components, and update home screen layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,65 +1,134 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import React, { useEffect, useState } from "react";
+import {
+  Image,
+  StyleSheet,
+  Platform,
+  SafeAreaView,
+  View,
+  Text,
+} from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import Corazon from "../../assets/images/svg/corazon.svg";
+import BolsaClinica from "../../assets/images/svg/bolsaClinica.svg";
+import Huesos from "../../assets/images/svg/huesos.svg";
+import Face from "../../assets/images/svg/face.svg";
 
 export default function HomeScreen() {
+  const [emergencyKinds, setEmergencyKinds] = useState([]);
+  const [selectedValue, setSelectedValue] = useState("");
+
+  useEffect(() => {
+    fetch(
+      "https://wisemed-interview.s3.us-east-2.amazonaws.com/react-native/emergency-kinds.json"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setEmergencyKinds(data.emergencyKinds);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#f3edf7" }}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <View style={styles.subContainer}>
+          <View style={{ width: 276 }}>
+            <View style={styles.titleContainer}>
+              <View
+                style={{ display: "flex", flexDirection: "column", gap: 10 }}
+              >
+                <Text style={styles.title}>Traumatología</Text>
+                <Text style={styles.subTitle}>Dr. José Pedro Sans</Text>
+              </View>
+              <Huesos width={50} height={50} />
+            </View>
+
+            <View style={styles.subTitleContainer}>
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  gap: 10,
+                }}
+              >
+                <Face width={24} height={24} />
+                <View
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: 10,
+                  }}
+                >
+                  <Text style={styles.titleName}>Jorge Avendaño Pérez</Text>
+                  <Text style={styles.titleName}>35 años</Text>
+                </View>
+              </View>
+              <View style={{ display: "flex", flexDirection: "row", gap: 5 }}>
+                <Text style={styles.fichaText}>Ficha médica:</Text>
+                <Text style={styles.fichaText2}>77884</Text>
+              </View>
+              <View style={{ display: "flex", flexDirection: "row", gap: 5 }}>
+                <Text style={styles.fichaText}>Diagnóstico:</Text>
+                <Text style={styles.fichaText2}>Calcificación Talón</Text>
+              </View>
+              <View style={{ display: "flex", flexDirection: "row", gap: 5 }}>
+                <Text style={styles.fichaText}>Intervención:</Text>
+                <Text style={styles.fichaText2}>Extirpación en talón</Text>
+              </View>
+              <View style={{ display: "flex", flexDirection: "row", gap: 5 }}>
+                <Text style={styles.fichaText}>Evaluación preanestésica:</Text>
+                <Text style={styles.fichaText2}>Sí</Text>
+              </View>
+              <View style={{ display: "flex", flexDirection: "row", gap: 5 }}>
+                <Text style={styles.fichaText}>Tiempo de solicitud:</Text>
+                <Text style={styles.fichaText2}>3 días</Text>
+              </View>
+              <View style={{ display: "flex", flexDirection: "row", gap: 5 }}>
+                <Text style={styles.fichaText}>Suspensiones:</Text>
+                <Text style={styles.fichaText2}>2</Text>
+              </View>
+
+              <View style={{ display: "flex", flexDirection: "row", gap: 10 }}>
+                <Corazon width={24} height={24} />
+                <BolsaClinica width={24} height={24} />
+              </View>
+
+              <View
+                style={{
+                  borderColor: "#154FBF",
+                  borderWidth: 1,
+                  borderRadius: 6,
+                  overflow: "hidden",
+                }}
+              >
+                <Picker
+                  selectedValue={selectedValue}
+                  onValueChange={(itemValue) => setSelectedValue(itemValue)}
+                  style={{
+                    height: 50,
+                  }}
+                >
+                  <Picker.Item label="Seleccionar" value="" />
+                  {emergencyKinds.map((item, index) => {
+                    return (
+                      <Picker.Item
+                        key={index}
+                        label={item.name || "Nombre no disponible"}
+                        value={item.name || ""}
+                      />
+                    );
+                  })}
+                </Picker>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
   stepContainer: {
     gap: 8,
     marginBottom: 8,
@@ -69,6 +138,56 @@ const styles = StyleSheet.create({
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
+  },
+
+  fichaText: {
+    fontSize: 12,
+    color: "#154FBF",
+    fontFamily: "PoppinsRegular",
+  },
+  fichaText2: {
+    fontSize: 12,
+    color: "#000000",
+    fontFamily: "PoppinsRegular",
+  },
+  title: {
+    fontSize: 16,
+    color: "white",
+    fontFamily: "PoppinsSemiBold",
+  },
+  subTitle: {
+    fontSize: 12,
+    color: "white",
+    fontFamily: "PoppinsSemiBold",
+  },
+  titleName: {
+    fontSize: 14,
+    color: "#000000",
+    fontFamily: "PoppinsMedium",
+  },
+  titleContainer: {
+    backgroundColor: "#154FBF",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 16,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+  },
+  subTitleContainer: {
+    paddingTop: 16,
+    display: "flex",
+    flexDirection: "column",
+    gap: 10,
+    padding: 16,
+  },
+  subContainer: {
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "white",
+    borderRadius: 5,
+    borderColor: "#154FBF",
+    borderWidth: 1,
   },
 });
